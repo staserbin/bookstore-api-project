b13df5f5fab225b2fb6c905ad0d593fa7f6c2145
Stan_Git <stan@gmail.com>
2026-02-08 20:08:47 +0200

    Added RestAssured timeout configurations to RestClient.java

--- src/main/java/com/bookstore/api/client/RestClient.java
+++ src/main/java/com/bookstore/api/client/RestClient.java
@@ -2,17 +2,27 @@ package com.bookstore.api.client;
 
 import com.bookstore.api.config.TestConfig;
 import io.restassured.RestAssured;
+import io.restassured.config.HttpClientConfig;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 
 public class RestClient {
 
+    static {
+        RestAssured.config = RestAssured.config()
+                .httpClient(HttpClientConfig.httpClientConfig()
+                        .setParam("http.connection.timeout", 10000)
+                        .setParam("http.socket.timeout", 10000)
+                );
+    }
+
     protected RequestSpecification defaultRequest() {
         return RestAssured.given()
+                .relaxedHTTPSValidation()
                 .baseUri(TestConfig.BASE_URL)
                 .accept(ContentType.JSON)
-                .log().all();
+                .header("Connection", "close");
     }
 
     protected RequestSpecification requestWithBody(Object body) {
